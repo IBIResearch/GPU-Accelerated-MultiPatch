5b282e84cf1696609662a188d134bab876a2281e
nHackel <nHackel@users.noreply.github.com>
2025-02-23 11:13:16 +0100

    Add gpu backend comparison

---
+++ compareGPUBackends.jl
@@ -0,0 +1,55 @@
+using MPIFiles, MPIReco, BenchmarkTools
+include("config.jl")
+
+sf = MultiMPIFile([joinpath(datadir, "SF$i$sm_suffix.mdf") for i = 1:4])
+meas = MPIFile(joinpath(datadir, "measStatic.mdf"))
+
+# We do a reoncstruction with one iteration and then retrieve the operator from the algortihm struct
+params = Dict{Symbol, Any}()
+params[:SNRThresh] = 2
+params[:minFreq] = 80e3
+params[:sf] = sf
+params[:recChannels] = 1:3
+params[:tfCorrection] = false
+params[:reg] = [L2Regularization(0.01)]
+params[:solver] = Kaczmarz
+params[:arrayType] = cpu
+params[:iterations] = 1
+params[:mapping] = collect(1:4)
+
+using MPIReco.AbstractImageReconstruction
+plan = loadPlan("./reco.toml", [AbstractImageReconstruction, MPIFiles, MPIReco])
+setAll!(plan, params)
+algo = build(plan)
+reconstruct(algo, meas)
+op = algo.ffOp
+
+# Load preprocessed measurement data
+u = AbstractImageReconstruction.process(algo, algo.params.pre, meas, algo.freqs)
+u = vec(u[:, :, 1]) # just first frame
+
+# Now we adapt and move operator and measurement data to the GPU
+using Adapt
+
+op_gpu = adapt(gpu, op)
+u_gpu = adapt(gpu, u)
+
+tmp = adjoint(op) * u
+tmp_gpu = adjoint(op_gpu) * u_gpu
+@assert isapprox(tmp, Array(tmp_gpu))
+@assert isapprox(op * tmp, Array(op_gpu * tmp_gpu))
+timesForwardCPU = @benchmark $op * $tmp seconds = (60 * 15) samples = trials evals = 1
+timesForwardGPU = @benchmark $op_gpu * $tmp_gpu seconds = (60 * 15) samples = trials evals = 1
+timesAdjointCPU = @benchmark $adjoint(op) * $u seconds = (60 * 15) samples = trials evals = 1
+timesAdjointGPU = @benchmark $adjoint(op_gpu) * $u_gpu seconds = (60 * 15) samples = trials evals = 1
+
+if save_results
+  using HDF5
+  mkpath(joinpath(resultdir, "data"))
+  h5open(joinpath(resultdir, "data", "backends.h5"), "cw") do file
+    write(file, "$gpu/forward/$cpu", timesForwardCPU.times ./ 1e9)
+    write(file, "$gpu/forward/$gpu", timesForwardGPU.times ./ 1e9)
+    write(file, "$gpu/adjoint/$cpu", timesAdjointCPU.times ./ 1e9)
+    write(file, "$gpu/adjoint/$gpu", timesAdjointGPU.times ./ 1e9)
+  end
+end
---
+++ makeFigureGPUBackend.jl
@@ -0,0 +1,47 @@
+using HDF5, CairoMakie, Colors
+
+Makie.update_theme!(fonts = (regular = Makie.texfont(), bold = Makie.texfont(:bold), italic = Makie.texfont(:italic)))
+
+colors = [RGB(178/255, 34/255, 41/255), RGB(104/255, 195/255, 205/255), RGB(138/255, 189/255, 36/255), RGB(239/255, 123/255, 5/255), RGB(0/255,73/255,146/255)]
+mkpath(joinpath(resultdir, "figures"))
+
+operatorfile = joinpath(resultdir, "data", "backends.h5")
+if isfile(operatorfile)
+  fig = Figure(backgroundcolor = :transparent)
+
+  h5open(operatorfile) do file
+    backends = fill("CuArray", 3) # keys(file)
+    backendMap = Dict(
+      "CuArray" => "CUDA",
+      "MtlArray" => "Metal",
+      "ROCArray" => "AMD"
+    )
+
+    ax = CairoMakie.Axis(fig[1, 1], backgroundcolor = :transparent, title = "GPU Backends", xticks = (1:length(backends), map(backend -> get(backendMap, backend, backend), backends)), ylabel = "Average Speedup", height = 150, width = 150)
+
+
+    for (i, backend) in enumerate(backends)
+      timesForwardCPU = file[backend]["forward"]["$cpu"][]
+      timesForwardGPU = file[backend]["forward"][backend][]
+      timesAdjointCPU = file[backend]["adjoint"]["$cpu"][]
+      timesAdjointGPU = file[backend]["adjoint"][backend][]
+
+      incr_forward = mean(timesForwardCPU) / mean(timesForwardGPU)
+      incr_adjoint = mean(timesAdjointCPU) / mean(timesAdjointGPU)
+
+      cat = [i, i]
+      dodge = [1, 2]
+      height = [incr_forward, incr_adjoint]
+      barplot!(ax, cat, height, dodge = dodge, color = colors[dodge .+ 3])
+  
+    end
+    elements = [PolyElement(polycolor = x) for x in colors[[1, 2] .+ 3]]
+    boxLegend = Legend(fig[1, 2], elements, ["Forward", "Adjoint"], orientation = :vertical, backgroundcolor = :transparent,)
+
+
+
+    resize_to_layout!(fig)
+    save(joinpath(resultdir, "figures", "backends.pdf"), fig)
+  end
+  fig
+end
